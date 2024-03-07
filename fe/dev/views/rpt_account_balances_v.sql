a36de2ec5e85f17f5e2715095fee546b48ebb23b
Dmitry Vinter <winterdmitrij@gmail.com>
2024-03-07 07:42:06 +0100

    Bal-Views angepasst

---
+++ fe/dev/views/rpt_account_balances_v.sql
@@ -0,0 +1,16 @@
+-- Balanz-Ansicht. benutzt zum jahres Pivot-Ansicht
+-- ToDo: Später vllt. umbauen ohne Tabelle rpt_balance
+SELECT cav.rank                               AS rank, 
+       bal.prd                                AS prd, 
+       cav.ag_id                              AS ag_id, 
+       cav.ag_dsg                             AS ag_dsg, 
+       cav.ad_id                              AS ad_id, 
+       cav.ad_dsg                             AS ad_dsg, 
+       bal.beg                                AS beg, 
+       bal.inc                                AS inc, 
+       bal.exp                                AS exp, 
+       bal.tra                                AS tra, 
+      (bal.inc + bal.exp + bal.tra)           AS sld, 
+      (bal.beg + bal.inc + bal.exp + bal.tra) AS [end]
+FROM  (rpt_balance         AS bal 
+INNER  JOIN cat_accounts_v AS cav ON cav.ad_id = bal.ad_id);
